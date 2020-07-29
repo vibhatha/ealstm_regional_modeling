b3e67823d3b988862c45284e81d3da5738064569
fugangwang <kevinwangfg@gmail.com>
2020-07-29 10:37:42 -0400

    removing the models that we are not testing

--- papercode/plotutils.py
+++ papercode/plotutils.py
@@ -193,22 +193,22 @@ model_specs = {
         'model': 'ealstm',
         'loss': 'NSELoss'
     },
-    'lstm_MSE': {
-        'model': 'lstm',
-        'loss': 'MSELoss'
-    },
-    'lstm_NSE': {
-        'model': 'lstm',
-        'loss': 'NSELoss'
-    },
-    'lstm_no_static_MSE': {
-        'model': 'lstm_no_static',
-        'loss': 'MSELoss'
-    },
-    'lstm_no_static_NSE': {
-        'model': 'lstm_no_static',
-        'loss': 'NSELoss'
-    }
+    #'lstm_MSE': {
+    #    'model': 'lstm',
+    #    'loss': 'MSELoss'
+    #},
+    #'lstm_NSE': {
+    #    'model': 'lstm',
+    #    'loss': 'NSELoss'
+    #},
+    #'lstm_no_static_MSE': {
+    #    'model': 'lstm_no_static',
+    #    'loss': 'MSELoss'
+    #},
+    #'lstm_no_static_NSE': {
+    #    'model': 'lstm_no_static',
+    #    'loss': 'NSELoss'
+    #}
 }
 
 attribute_draw_style = {
