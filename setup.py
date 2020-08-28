2e451837b665aa1650a51e64a1c171f065733518
vibhatha <vibhatha@gmail.com>
2020-08-28 12:45:10 -0400

    adding a setup.py for ease in installation

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ealstm_regional_modeling",  # Replace with your own username
+    version="0.0.1",
+    author="Kratzert, F., Klotz, D., Shalev, G., Klambauer, G., Hochreiter, S., and Nearing, G.",
+    author_email="",
+    description="EALSTM Packaging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kratzert/ealstm_regional_modeling",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
